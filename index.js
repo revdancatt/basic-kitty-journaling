7a9373dfd6735abf0f3f44a33f0ef6c458d7c75b
revdancatt <github@revdancatt.com>
2023-08-23 10:33:20 +0100

    Improve question dating, make sure to have the default questions in there

--- index.js
+++ index.js
@@ -27,6 +27,28 @@ const gptCompletion = async (messages, model, openai, temp = 0.5, topP = 1.0, to
   return text
 }
 
+// Get a rough count of the tokens in what we've been sent
+const getTokenCount = async (messages) => {
+  const count = await encode(JSON.stringify(messages)).length
+  return count
+}
+
+// Work out the cost of the tokens that we are sending to gpt
+// based on 'gpt-4' costs
+const getTokenInputCost = async (messages) => {
+  const tokenCost = await getTokenCount(messages) / 1000 * 0.03
+  // We want to format this as a dollar amount
+  return `${tokenCost.toFixed(2)}`
+}
+
+// Work out the cost of the tokens we are getting back from gpt
+// based on 'gpt-4' costs
+const getTokenOutputCost = async (messages) => {
+  const tokenCost = await getTokenCount(messages) / 1000 * 0.06
+  // We want to format this as a dollar amount
+  return `${tokenCost.toFixed(2)}`
+}
+
 const main = async () => {
   // Check for a data folder and if it doesn't exist create it
   const dataFolder = path.join(__dirname, 'data')
@@ -105,48 +127,85 @@ const main = async () => {
     }
   )
 
+  messages.push(
+    {
+      role: 'system',
+      content: `Here is a list of default questions to get you started\n${dataJSON.morningQuestions.join('\n')}`
+    }
+  )
+
   // Now we're going to grab the last five entires from the previousQuestionsAndAnswers object
   // This next line grabs the last five and then reverses them so we'll have the most recent one first
   // Go grab the previous questions and answers
-  let previousQuestionsAndAnswers = {}
-  const previousQuestionsAndAnswersPath = path.join(dataFolder, 'previousQuestionsAndAnswers.json')
-  if (fs.existsSync(previousQuestionsAndAnswersPath)) previousQuestionsAndAnswers = JSON.parse(fs.readFileSync(previousQuestionsAndAnswersPath))
-  let lastFiveDates = []
-  if (previousQuestionsAndAnswers.dateMap) lastFiveDates = previousQuestionsAndAnswers.dateMap.slice(-5).reverse()
-  const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  let daysAgoCounter = 1
+  let questionsAndAnswers = {}
+  const questionsAndAnswersFile = path.join(dataFolder, 'previousQuestionsAndAnswers.json')
+  if (fs.existsSync(questionsAndAnswersFile)) questionsAndAnswers = JSON.parse(fs.readFileSync(questionsAndAnswersFile))
+
+  // Grab the last five dates from the dateMap array if there are that many
+  // until we have enough dates
+  const days = 5
+  const lastFiveDates = []
+  let pastDatesNewToOld = []
+  if (questionsAndAnswers.dateMap) pastDatesNewToOld = questionsAndAnswers.dateMap.reverse()
+
   // Now loop through those dates so we can grab the questions and answers from the answers node
+  for (let i = 0; i < pastDatesNewToOld.length; i++) {
+    // If there are any answers for this date, add it to the array
+    if (questionsAndAnswers.answers[pastDatesNewToOld[i]] && questionsAndAnswers.answers[pastDatesNewToOld[i]].length) {
+      lastFiveDates.push(pastDatesNewToOld[i])
+    }
+    // If we have enough dates, break out of the loop
+    if (lastFiveDates.length === days) break
+  }
+
+  // For reasons
+  const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+  // get today's date in YYYY-MM-DD format
+  const today = new Date().toISOString().slice(0, 10)
+
+  const previousQuestionsAndAnswers = []
   for (const date of lastFiveDates) {
+    // The date is in the format of YYYY-MM-DD, work out how many days ago it was from today
+    // using the variable 'today' to compare it too
+    const daysAgo = Math.floor((Date.parse(today) - Date.parse(date)) / 86400000)
     // The date is in the format of YYYY-MM-DD, we need to convert that into a day of the week
     const dayOfTheWeek = daysOfTheWeek[new Date(date).getDay()]
-    // See if we have an entry for this date
-    if (previousQuestionsAndAnswers.answers[date]) {
+    // If it exists, we need to loop through them
+    if (questionsAndAnswers.answers[date]) {
       // Loop through those question answer objects
-      let previousQuestion = ''
-      if (daysAgoCounter === 1) {
-        previousQuestion += `Yesterday, ${dayOfTheWeek} (${date}), ${dataJSON.name} was asked the following questions: `
+      let daysQandAs = ''
+      if (daysAgo === 0) {
+        daysQandAs += `Today, ${dayOfTheWeek} (${date}), ${dataJSON.name} Q&As were:\n`
       } else {
-        if (daysAgoCounter === 2) {
-          previousQuestion += `${dayOfTheWeek} just gone (${date}), ${dataJSON.name} was asked the following questions: `
+        if (daysAgo === 1) {
+          daysQandAs += `Yesterday, ${dayOfTheWeek} (${date}), ${dataJSON.name} Q&As were:\n`
         } else {
-          previousQuestion += `Last ${dayOfTheWeek} (${date}), ${dataJSON.name} was asked the following questions: `
+          if (daysAgo === 2) {
+            daysQandAs += `${dayOfTheWeek} just gone (${date}), ${dataJSON.name} Q&As were:\n`
+          } else {
+            daysQandAs += `Last ${dayOfTheWeek} (${date}), ${dataJSON.name} Q&As were:\n`
+          }
         }
       }
-      for (const questionAnswer of previousQuestionsAndAnswers.answers[date]) {
-        previousQuestion += `${questionAnswer.question} - `
+
+      for (let i = 0; i < questionsAndAnswers.answers[date].length; i++) {
+        daysQandAs += `Q${i + 1}: ${questionsAndAnswers.answers[date][i].question}\n`
+        daysQandAs += `A${i + 1}: ${questionsAndAnswers.answers[date][i].answer}\n`
       }
-      previousQuestion += 'and gave the following answers: '
-      for (const questionAnswer of previousQuestionsAndAnswers.answers[date]) {
-        previousQuestion += `${questionAnswer.answer} - `
-      }
-      messages.push(
-        {
-          role: 'system',
-          content: previousQuestion
-        }
-      )
+      daysQandAs += '\n'
+      previousQuestionsAndAnswers.push(daysQandAs)
     }
-    daysAgoCounter++
+  }
+
+  // Now we have the previous questions and answers we need to add them to the messages array
+  for (const questionAnswer of previousQuestionsAndAnswers) {
+    messages.push(
+      {
+        role: 'system',
+        content: questionAnswer
+      }
+    )
   }
 
   // Now we've set all of that up, the meta prompt, the day of the week, the previous questions and answers
@@ -169,8 +228,7 @@ const main = async () => {
     // empty the questions array
     questions.length = 0
     if (escapeCounter === 0) {
-      const tokenCount = encode(JSON.stringify(messages)).length
-      term.cyan('\nThinking of some questions to ask you, please wait. (').yellow(`~${tokenCount} tokens`).cyan(')\n')
+      term.cyan('\nThinking of some questions to ask you, please wait. (').yellow(`${await getTokenCount(messages)} tokens/$${await getTokenInputCost(messages)}`).cyan(')\n')
     } else {
       term.cyan('Getting some different questions\n')
     }
@@ -178,7 +236,10 @@ const main = async () => {
 
     // Lazy error/exception handling, you'd want to make this better!
     try {
+      const startTime = new Date().getTime()
       output = await gptCompletion(messages, 'gpt-4', openai)
+      const durationInSeconds = Math.floor((new Date().getTime() - startTime) / 1000)
+      term.cyan('Response: ').yellow(`${await getTokenCount(output)} tokens/$${await getTokenOutputCost(output)} ${durationInSeconds}s\n`)
     } catch (err) {
       console.log(err)
       process.exit()
@@ -226,19 +287,19 @@ const main = async () => {
   const date = d.toISOString().slice(0, 10)
   // Now we need to see if we have an entry for this date in the previousQuestionsAndAnswers.json file,
   // if not add it to the dateMap array and create an empty answers array
-  if (!previousQuestionsAndAnswers.answers) previousQuestionsAndAnswers.answers = {}
-  if (!previousQuestionsAndAnswers.dateMap) previousQuestionsAndAnswers.dateMap = []
+  if (!questionsAndAnswers.answers) questionsAndAnswers.answers = {}
+  if (!questionsAndAnswers.dateMap) questionsAndAnswers.dateMap = []
   // If the date is not in the dateMap array then add it
-  if (!previousQuestionsAndAnswers.dateMap.includes(date)) previousQuestionsAndAnswers.dateMap.push(date)
+  if (!questionsAndAnswers.dateMap.includes(date)) questionsAndAnswers.dateMap.push(date)
   // Clear the answers array (or create it if it doesn't exist)
-  previousQuestionsAndAnswers.answers[date] = []
+  questionsAndAnswers.answers[date] = []
 
   // Now we need to add the questions and answers to the answers array, do this by looping through
   // the questions array and adding the question and answer to the answers array
   for (let i = 0; i < questions.length; i++) {
     // Only add it if the answer is not empty
     if (answers[i].trim() !== '') {
-      previousQuestionsAndAnswers.answers[date].push(
+      questionsAndAnswers.answers[date].push(
         {
           question: questions[i],
           answer: answers[i]
@@ -247,7 +308,7 @@ const main = async () => {
     }
   }
   // Now we need to save the previousQuestionsAndAnswers object to the previousQuestionsAndAnswers.json file
-  fs.writeFileSync(path.join(dataFolder, 'previousQuestionsAndAnswers.json'), JSON.stringify(previousQuestionsAndAnswers, null, 2))
+  fs.writeFileSync(path.join(dataFolder, 'previousQuestionsAndAnswers.json'), JSON.stringify(questionsAndAnswers, null, 2))
   // Thank the user and wish them a good day
   term.cyan('\nThank you for your time, have a good day.\n')
   process.exit()
